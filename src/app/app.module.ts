82ebea24fa15e5e3d95bb1cc4f815e857d03a78a
Bruna Roberta Gil <brunagil.eppen@gmail.com>
2019-01-31 16:06:01 -0200

    upload images and create components folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component'
+import { HeaderComponent } from './components/header/header.component'
+import { FooterComponent } from './components/footer/footer.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ //declara todas as diretivas que vamos utilizar nesse módulo
+    AppComponent, NavbarComponent, HeaderComponent, FooterComponent, CardsComponent, FormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], //serviços que vão ficar disponíveis para todos os módulos; escopo global da aplicação (router)
+  bootstrap: [AppComponent] //componente que serve como container para nosso projeto
 })
 export class AppModule { }
