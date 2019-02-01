dac0ffc0c0609279d26325ff899f64f037a7eed2
Bruna Roberta Gil <brunagil.eppen@gmail.com>
2019-01-31 22:19:18 -0200

    create cards content and update style w/ responsive layout

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.scss']
 })
-export class CardsComponent implements OnInit {
 
-  constructor() { }
+export class CardsComponent {
+  @Input() cardPosition: string;
 
-  ngOnInit() {
+  classByPosition() {
+    const cardClasses = {
+      left: 'card_left',
+      center: 'card_center',
+      right: 'card_right'
   }
-
+  return cardClasses[this.cardPosition];
+  }
 }
